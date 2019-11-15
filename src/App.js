42e9eb089cb2fb4d218fe56feffb05bef4a0292b
adrian1313 <55831474+adrian1313@users.noreply.github.com>
2019-11-15 15:39:28 +0800

    my First JS beginner

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from "react";
+import Tweet from "./Tweet";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App (){
+
+const [users, setUsers] = useState([
+  {name: 'AJ', message: 'Hello there'},
+  {name: 'CJ', message: 'I am CJ'},
+  {name: 'Traversy', message: 'I am Travers'}
+]);
+
+  return(
+    <div className = "app">
+      {users.map(user => (
+        <Tweet name={user.name} message = {user.message}/>
+      ))}
     </div>
   );
 }
 
+// const [user, setUser] = userState({
+//   name: "Ed",
+//   age: 25,
+//   posts: ['my first post' , 'my summer']
+// });
+
+// const decrement = () => {
+//   setCount(count - 1);
+// }
 export default App;
